9b59cd56f11d6e536410a25d4a0adf9d54e0efc0
manyabisht <imanya1007@gmail.com>
2026-05-31 00:27:33 +0530

    Fix: Configure native PDO parameters for multi-tenant pooler routing

--- config/database.php
+++ config/database.php
@@ -97,6 +97,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => [
+            PDO::Connect_Option_Name => 'reference=htebgcbfaswzdvwcxtyd',
+        ]
         ],
 
         'sqlsrv' => [
